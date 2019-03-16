c5e65d8033f972e6219ff51998dd2ececfca0fd7
Andrew Erb <ase111@gmail.com>
2019-03-16 01:40:22 -0400

    API Models commenting

--- apps/wordapi/models.py
+++ apps/wordapi/models.py
@@ -2,12 +2,13 @@
 
     For Django fc_coding_challenge - wordapi app.
 
-    - This model exist for dictionaries of words, across lanaugages/dictionaries, to be quieried by an API
-    - This model and API is for finding substring matches to a word in data, 
-    - All words that are anagrams of one another will also share the same alphagram– the resulting string from sorting a word/string's letters/characters alphabetically.  https://en.wiktionary.org/wiki/alphagram
-    - Alphagrams are used here to assocate words in data that are anagrams.
+    - This model exist for querying words, across lanaugages/dictionaries, via API.
+    - The intended usecase of this model and API is to find word-matches to substring user-queries, and anagrams of those matching words.
+    - Alphagrams are used here to assocate words in data that are anagrams to each other.
+    - All words that are anagrams of one another will also share the same alphagram– the string resulting from sorting a word/string's letters/characters alphabetically.  https://en.wiktionary.org/wiki/alphagram
 
     Model overview:
+    TODO
 
 """
 
@@ -20,6 +21,8 @@ from django.utils import timezone
 
 #   Data Models
 class WordLanguage(models.Model):
+    """ WordLanguage is the dictionary language, associated with words by a many-to-one relationship. The name is cumbersome due to the vague nature of the word 'language'.
+    """
     label = models.CharField(max_length=30, unique=True)
     created_date = models.DateTimeField(auto_now_add=True)
 
@@ -27,12 +30,16 @@ class WordLanguage(models.Model):
         return self.label
 
 class Alphagram(models.Model):
+    """ Alphagrams for association with words by a many to one relationship. Primarily used to find words' anagrams.
+    """
     label = models.CharField(max_length=50, unique=True)
     
     def __str__(self):
         return self.label
 
 class Word(models.Model):
+    """ Words from dictionary list.
+    """
     label = models.CharField(max_length=50) # Not unique. Other words (homographs) may have same spelling, but unique IDs.
     alphagram = models.ForeignKey(Alphagram, on_delete=models.CASCADE)
     
