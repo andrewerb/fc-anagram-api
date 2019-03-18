ab37b2b6b9f74c773f46ef61c885a8bf9047435a
Andrew Erb <ase111@gmail.com>
2019-03-18 02:06:14 -0400

    API views for substring matching w/ params, and anagram search. Slightly WIP.

--- apps/wordapi/urls.py
+++ apps/wordapi/urls.py
@@ -1,6 +1,11 @@
 """ URL routing for API - wordAPI
     
     API level routers and index path.
+
+    TODO: 
+        Backup 'api/' path to config URLs; and move templates?
+        Elaborate on paths & routing
+        Fix a lot of routes
 """
 
 
@@ -9,21 +14,30 @@ from rest_framework import routers
 from . import views
 
 
+# Django Rest routing for API - publicly visible at API index:
 router = routers.DefaultRouter()
 router.register(r'words', views.WordViewSet) # aliases? plural
 router.register(r'languages', views.LanguageViewSet)
-router.register(r'substring', views.WordViewSet) # matches # temporarily using word views. URL param filter needed
-#router.register(r'words/sub', views.WordViewSet)
-'''
-router.register(r'anagram', views.WordViewSet)
-router.register(r'substr_anagrams', views.WordViewSet)
-'''
+# TODO: stub None view for redundancy - substrings, anagrams, anagramsbysubstring, None / 404
+# TODO: singular wording for redundancy
+
+# router.register(r'substring', views.WordViewSet)
+# router.register(r'words', views.WordViewSet)
 
 
 urlpatterns = [
-    #path('api/test', views.LanguageViewSet),#re_path - testing for regexp for aliases, and staying off index list. +param (fine in router!)
-    re_path(r'api/alphagrams/', views.AlphagramQuery.as_view(), name="alphagrams"),
-    path('', views.index, name='index'),
-    path('api/', include(router.urls)),
-] # todo: 404
+    
+    # ex: /api/substrings/foo
+    re_path(r'^api/substrings?\/(?P<search_id>.+)/$', views.WordBySubstringView.as_view(), name="substrings"),
+    re_path(r'api/anagrams?\/', views.AnagramView.as_view(), name="anagrams"),
+    # router.register(r'substr_anagrams', views.WordViewSet)  #  TODO!
+    # TODO: word router by label
+    
 
+    # For testing:
+    re_path(r'api/alphagrams?\/', views.AlphagramView.as_view(), name="alphagrams"),
+    
+    # Non-API paths:
+    path('', views.index, name='index'), ## TODO: Move up to base and point to app views and templates. Drop 'API/' everywhere.
+    path('api/', include(router.urls)),
+]
--- apps/wordapi/views.py
+++ apps/wordapi/views.py
@@ -1,4 +1,6 @@
-""" Word API Views
+""" Dictionary-Words / Anagram API app Views
+
+    API endpoints only have GET methods in this app iteration, no additional HTTP request methods.
 """
 
 
@@ -13,24 +15,23 @@ from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from .serializers import * #UserSerializer, GroupSerializer
+from .serializers import *
 from .models import *
 
 
+def response_404_none():
+    """ Return "None" and 404 status code
+        For GET requests from API endpoints, where/when queries yield no result
+    """
+    return Response("None", status=status.HTTP_404_NOT_FOUND)
+
+
+##  API Views  ##
 class WordViewSet(viewsets.ModelViewSet):
-    """ API endpoint that allows words to be viewed or edited. # TODO!
+    """ API endpoint for dictionary words
     """
     queryset = Word.objects.all().order_by('label')
     serializer_class = WordSerializer
-
-
-class AlphagramQuery(APIView):
-    """ Returns a list of all alphagrams
-    """
-    def get(self, request, format=None):
-        alpha_set = Alphagram.objects.all()
-        serializer = AlphaSerializer(alpha_set, many=True)
-        return Response(serializer.data)
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
@@ -38,8 +39,78 @@ class LanguageViewSet(viewsets.ModelViewSet):
     """
     queryset = WordLanguage.objects.all()
     serializer_class = LanguageSerializer
-    
 
+
+# Words Bylabel
+
+
+class WordBySubstringView(APIView):
+    def get(self, request, format=None, search_id=""):
+        """ API's GET method, for substring-input
+
+            Queries Word model (dictionary words) for set of Words for which input is a substring.
+        """
+        substr_input = search_id
+        
+        try:
+            # Query filtering for case-insentive containment of the input-string (as a substring) in Word entry labels
+            queryset = Word.objects.filter(label__icontains=substr_input)
+        except:
+            # 404/"None" if no results
+            return response_404_none()
+
+        # Continue if queryset found
+        serializer = WordSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+class AnagramView(APIView): # TODO - URL param
+    """ Anagram fetching by Subject-Word label
+        
+        Queries alphagram of subject-word, filters for words associated, and returns that list sans the subject-word.
+        Returns a set of Word objects
+    """
+    def get(self, request, format=None): ## TODO!!! TAKE PARAM FROM URL ROUTER
+
+        # TODO: regexp check for alpha chars useful here
+        label_input = "listen" # TODO - UPDATE!!! PARAMS, for non-exist - try, or filter; combine/join!!
+        
+        if label_input: # If input isn't blank
+            try:
+                subject_word = Word.objects.get(label=label_input) # Check for subject-word
+            except:
+                # Subject word doesn't exist. No anagrams. Return none.
+                return response_404_none()
+            
+            # If subject-word object was found
+            queryset = subject_word.alphagram.word_set.all().exclude(id=subject_word.id)  # The anagram queryset is the set of words sharing the same alphagram as the subject-word, excluding the subject-word itself.
+            
+            serializer = WordSerializer(queryset, many=True)
+            return Response(serializer.data)
+
+            # TODO: params, combine/join
+        else:
+            return Response("None") ## 404!!
+
+class AnagramBySubstringView(APIView): # TODO
+    def get(self, request, format=None):
+        pass
+        ## 1st: regular anagra - query params
+        ## test random or appended list
+
+class AlphagramView(APIView): # TODO: Fix 404
+    """ Returns a list of all alphagrams
+
+        TODO: FIX THIS A LOT! - errors?
+    """
+    def get(self, request, format=None):
+        queryset = Alphagram.objects.all()
+        serializer = AlphaSerializer(queryset, many=True)
+        return Response(serializer.data)
+
+
+####################################        
+##  Web Views (non-API)  ##
 def index(request):
     #word_list = Word.objects.all()
     all_words = []
