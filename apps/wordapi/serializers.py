79b64db55f6a088cb030b29f7d0d79c396ed6ab8
Andrew Erb <ase111@gmail.com>
2019-03-21 16:32:56 -0400

    Serializers explained

--- apps/wordapi/serializers.py
+++ apps/wordapi/serializers.py
@@ -1,22 +1,36 @@
-""" Django Rest Framework Serializers for Word API
+""" Dictionary-Word/Anagram API - Data Serializers
+
+    - Seriealizers extend Django Rest Framework serializers classes
+    - Serializers create JSON http-responses from data-queries in views.py
+    - Serializers exist by model type, to handle that model's attributes accordingly. More than one can be made as needed by API views. 
+    - Fields specify what attributes/data-tables go into JSON responses in API.
 """
 
-#from django.contrib.auth.models import User, Group
+
 from rest_framework import serializers
 from .models import *
 
 
-class WordSerializer(serializers.HyperlinkedModelSerializer): # check type, ID/label
+class WordSerializer(serializers.ModelSerializer): # check type, ID/label # HyperlinkedModelSerializer
+    """ Word serializer - the most used for this app
+    """
     class Meta:
         model = Word
-        fields = ('url', 'id', 'label') # language-label, or print that ull object
+        fields = ('id', 'label')
+        # TODO: Display related language?
 
-class AlphaSerializer(serializers.ModelSerializer): # ALPHAGRAM serializer - WIP 
-    class Meta:
-        model = Alphagram
-        fields = ('id', 'label')#no url
 
-class LanguageSerializer(serializers.HyperlinkedModelSerializer):
+class LanguageSerializer(serializers.ModelSerializer):
+    """ Languages Serializer
+    """
     class Meta:
         model = WordLanguage
         fields = ('url', 'label')
+
+
+class AlphaSerializer(serializers.ModelSerializer):
+    """ Alphagram serializer, for testing. Not in API.
+    """
+    class Meta:
+        model = Alphagram
+        fields = ('id', 'label')
